bc94dd33de0ece93f9502da0fd0f871dfae28d94
isamalhariri98 <75997416+isamalhariri98@users.noreply.github.com>
2021-01-04 21:31:53 +0200

    Add files via upload

---
+++ uni.js
@@ -0,0 +1,120 @@
+ 
+//function isam(year) {
+   // var
+   // age=365;
+ //  return "Hi isam  your age in Day = " + year*age + "."
+//}
+//isam()
+//var year =21;
+//document.getElementById("vvvv").innerHTML=isam (prompt("What your age?"));
+//var human = prompt("Are You Doctor or student?");
+
+//switch (human) {
+  // case "Doctor": alert("Welcome Doctor .....");break;
+      // case "student": alert("Welcome student .....");break;
+        // case "isam": alert("راسب من هسا ف تتعبش حالك.....");break;
+         //   case "maha": alert("حبيبة دلب عصوم و عيونه");brea
+      // default: alert("عذرا صديقي");
+      
+      
+//}
+
+       // function isam() {
+         //       document.getElementById("text").innerHTML="hi  isam alhariri";
+           //     alert=("iasm jjj")
+            // }
+                
+     //function isam2() {
+         //       document.getElementById("text").innerHTML="hi  maha love";
+          //   }
+          
+          alert("تنويه: هذا الموقع خاص بطلبة جامعة العلوم والتكنولوجيا الأردنية.")
+          arr=["yosra","isam"];
+
+          var myInput=document.getElementById("in");
+          myCho=document.getElementById("ch");
+           myDiv=document.getElementById("di");
+           "use strict";
+          function calculate(){
+             myDiv .innerHTML=myInput.value*myCho.value;
+             return myDiv;
+             };
+             
+             
+             
+             
+             var
+             myIn=document.getElementById("input");
+              myChos=document.getElementById("cho");
+               myDi=document.getElementById("div");
+        function cal() {
+            myDi.innerHTML=myIn.value*myChos.value;
+            return myDi;
+        } ;
+        
+        
+        
+        var myInput3=document.getElementById("input3");
+        myChose3=document.getElementById("chose3");
+        myDiv3=document.getElementById("div3");
+        function three() {
+            myDiv3.innerHTML=myInput3.value*myChose3.value;
+            return myDiv3;
+        }
+        
+        
+        
+        
+        var myInput4=document.getElementById("input4");
+        myChose4=document.getElementById("chose4");
+        myDiv4=document.getElementById("div4");
+        function four() {
+            myDiv4.innerHTML=myInput4.value*myChose4.value;
+            return myDiv4;
+        }
+        
+        
+        
+        var myInput5=document.getElementById("input5");
+        myChose5=document.getElementById("chose5");
+        myDiv5=document.getElementById("div5");
+        function five() {
+            myDiv5.innerHTML=myInput5.value*myChose5.value;
+            return myDiv5;}
+          
+          
+          
+          
+          var myInput6=document.getElementById("input6");
+        myChose6=document.getElementById("chose6");
+        myDiv6=document.getElementById("div6");
+        function six() {
+            myDiv6.innerHTML=myInput6.value*myChose6.value;
+            return myDiv6;
+        }
+        
+        var numper=document.getElementById("Numper");
+        
+        
+        
+        
+        
+        
+        
+        myAv=document.getElementById("av");
+        function avg() {
+           myAv.innerHTML=(myInput.value*myCho.value+
+                     myIn.value*myChos.value+
+                     myInput3.value*myChose3.value+
+                     myInput4.value*myChose4.value+
+                     myInput5.value*myChose5.value+
+                     myInput6.value*myChose6.value)/numper.value;
+      
+           console.log(myAv);
+           
+        };
+        
+        
+        
+          
+          
